b6aa6e0ef745716d0cb149a5a3b2166ed0afaa19
Andrew Kittredge <andrew@calcbench.com>
2019-05-16 11:20:49 -0400

    initial commit

--- CalcbenchListener/Program.cs
+++ CalcbenchListener/Program.cs
@@ -1,13 +1,12 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
 
 namespace CalcbenchListener
 {
     class Program
     {
+        static IQueueClient queueClient;
         static void Main(string[] args)
         {
             // The code provided will print ‘Hello World’ to the console.
@@ -16,6 +15,25 @@ namespace CalcbenchListener
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        }
+
+        static async Task AsyncListenForFilings()
+        {
+            var connectionString = Properties.Settings.Default.ServiceBusConnectionString;
+            var queueName = Properties.Settings.Default.QueueName;
+            queueClient = new QueueClient(connectionString, queueName);
+            RegisterOnFilingHandlerAndReceiveMessages();
+            await queueClient.CloseAsync();
+        }
+
+        static void RegisterOnFilingHandlerAndReceiveMessages()
+        {
+            queueClient.RegisterMessageHandler()
+        }
+
+        static async Task ProcessFiling()
+        {
+
         }
     }
 }
