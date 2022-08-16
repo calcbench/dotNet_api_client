1461ecbbcd8afd30a75a259a3057552f19fa79b7
Andrew Kittredge <andrewlkittredge@gmail.com>
2022-08-16 09:44:08 -0400

    more work

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Calcbench;
 
 string[] companyIdentifers = { "MSFT", "ORCL" };
-APIClient client = new("https://localhost/api");
+APIClient client = new();
 var filings = await client.Filings(companyIdentifiers: companyIdentifers);
 Console.WriteLine(string.Join(",", filings.Select(f => $"{f.entity_name} {f.fiscal_year} {f.fiscal_period} {f.filing_type}")));
